ad8545bf7ce0992fd21bb5e8e2321a403fa56a01
Manuel Stanculovic <2058@htl.rennweg.at>
2026-03-01 21:03:11 +0100

    added form to new question

--- src/Controller/LessonController.php
+++ src/Controller/LessonController.php
@@ -20,38 +20,4 @@ final class LessonController extends AbstractController
         ]);
     } */
 
-    #[Route('/product', name: 'create_product')]
-    public function createProduct(EntityManagerInterface $entityManager): Response
-    {
-        $product = new Lesson();
-        $product->setTitle('Keyboard');
-        $product->setDuration(40);
-        $product->setDescription('Keyboard');
-
-
-        // tell Doctrine you want to (eventually) save the Product (no queries yet)
-        $entityManager->persist($product);
-
-        // actually executes the queries (i.e. the INSERT query)
-        $entityManager->flush();
-
-        return new Response('Saved new product with id '.$product->getId());
-    }
-
-        
-    #[Route('/product/{id}', name: 'app_question')]
-    public function index(EntityManagerInterface  $entityManager, int $id): Response
-    {
-
-    
-         $question = $entityManager->getRepository(Lesson::class)->find($id);
-        
-        if (!$question) {
-            throw $this->createNotFoundException(
-                'No product found for id '.$id
-            );
-        }
-
-        return new Response('Check out this great product: ' . $question->getText());
-    }
 }
--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -4,14 +4,42 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use App\Entity\Question;
-
-use Symfony\Component\HttpFoundation\Response;
+use App\Entity\Lesson;
+use Symfony\Component\HttpFoundation\Request; 
 use Symfony\Component\Routing\Attribute\Route;
 use Doctrine\ORM\EntityManagerInterface;
-
+use Symfony\Component\HttpFoundation\Response;
+use App\Form\QuestionType;
 
 final class QuestionController extends AbstractController
 {
+
+ #[Route('/question/new', name: 'app_question_new')]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $question = new Question();
+        $form = $this->createForm(QuestionType::class, $question);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $question = new Question();
+            $question->setText($form->get('text')->getData());
+            $question->setLesson($form->get('lesson')->getData());
+
+            $entityManager->persist($question);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_question', ['id' => $question->getId()]);
+        }
+
+        return $this->render('newQuestion.html.twig', [
+            'form' => $form,
+        ]);
+    }
+
+
+
     #[Route('/question/{id}', name: 'app_question')]
     public function index(EntityManagerInterface  $entityManager, int $id): Response
     {
